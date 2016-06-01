35115851e697c27b5a1b25bbec56592ed4f642ea
Emma Sjöström <emma.sjoestroem@gmail.com>
2016-06-01 22:20:59 +0200

    Added factory for ToDo

--- test/e2e/todosFeature.js
+++ test/e2e/todosFeature.js
@@ -1,31 +1,35 @@
-
 describe('Todos tracker', function() {
-  it('has a todo', function() {
+  it('has several ToDos', function() {
     browser.get('/');
     var todos = $$('#todos p');
-    expect(todos.first().getText()).toEqual('ToDo1: completed');
-    expect(todos.last().getText()).toEqual('ToDo2: not completed');
+    expect(todos.first().getText()).toMatch('ToDo1: completed');
+    expect(todos.last().getText()).toMatch('ToDo2: not completed');
   });
 
   it('can add a ToDo', function() {
     browser.get('/');
-    // sendKeys tells protractor to enter the string "NewToDo" into the input
-    $('#new-todo-name').sendKeys("NewToDo");
+    $('#new-todo-name').sendKeys("NewTodo");
     $('#add-todo').click();
 
     var todo = $$('#todos p').last().getText();
-    expect(todo).toEqual('NewToDo: not completed');
+    expect(todo).toMatch('NewTodo: not completed');
   });
 
   it('can remove a ToDo', function() {
     browser.get('/');
     var todos = $$('#todos p');
+    var initialCount = todos.count();
 
     $('#remove-todo').click();
 
-    // This has a magic number, how could this magic number be avoided?
-    // The solution is actually surprisingly complex so we've kept in the magic
-    // number for simplicity's sake
     expect(todos.count()).toEqual(1);
+  });
+
+  it('can mark a ToDo as complete', function(){
+    browser.get('/');
+    var todo = $$('#todos p').last();
+    todo.element(by.css('.complete')).click();
+
+    expect(todo.getText()).toMatch("ToDo2: completed");
   });
 });
--- test/unit/ToDoController.spec.js
+++ test/unit/ToDoController.spec.js
@@ -1,34 +1,33 @@
 describe('ToDoController', function() {
   beforeEach(module('toDoApp'));
 
-  var ctrl;
+  var ctrl, ToDoFactory;
 
-  beforeEach(inject(function($controller) {
+  beforeEach(inject(function($controller, _ToDoFactory_) {
     ctrl = $controller('ToDoController');
+    ToDoFactory = _ToDoFactory_;
   }));
 
-  it('initialises with several toDos', function() {
-    var todos = [
-      {text: "ToDo1", completed: true},
-      {text: "ToDo2", completed: false}
-    ];
+  it('initialises with several todos', function() {
+    var todo1 = new ToDoFactory("ToDo1", true);
+    var todo2 = new ToDoFactory("ToDo2", false);
 
-    expect(ctrl.todos).toEqual(todos);
+    expect(ctrl.todos).toEqual([todo1, todo2]);
   });
 
   it('adds a new todo', function() {
-    ctrl.addToDo('NewTodo');
+    ctrl.addToDo('NewToDo');
 
-    var todo = {text: "NewTodo", completed: false};
+    // Similarly this now uses a factory
+    var todo = new ToDoFactory("NewToDo");
     expect(ctrl.todos.pop()).toEqual(todo);
   });
 
   it('removes the last todo', function() {
-    var initialCount = ctrl.todos.length;
+    initialCount = ctrl.todos.length;
 
     ctrl.removeToDo();
 
     expect(ctrl.todos.length).toEqual(initialCount - 1);
   });
-
 });
