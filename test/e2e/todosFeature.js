b5b48bf10565bbb7f8f8277c749b15da13bc64bd
Emma Sjöström <emma.sjoestroem@gmail.com>
2016-06-04 18:30:27 +0200

    Users can search todos by name

--- test/e2e/todosFeature.js
+++ test/e2e/todosFeature.js
@@ -16,7 +16,7 @@ describe('Todos tracker', function() {
 
   it('has several ToDos', function() {
     browser.get('/');
-    var todos = $$('#todos p');
+    var todos = $$('#todos tr');
     expect(todos.first().getText()).toMatch('ToDo1: completed');
     expect(todos.last().getText()).toMatch('ToDo2: not completed');
   });
@@ -26,13 +26,13 @@ describe('Todos tracker', function() {
     $('#new-todo-name').sendKeys("NewTodo");
     $('#add-todo').click();
 
-    var todo = $$('#todos p').last().getText();
+    var todo = $$('#todos tr').last().getText();
     expect(todo).toMatch('NewTodo: not completed');
   });
 
   it('can remove a ToDo', function() {
     browser.get('/');
-    var todos = $$('#todos p');
+    var todos = $$('#todos tr');
     var initialCount = todos.count();
 
     $('#remove-todo').click();
@@ -42,10 +42,19 @@ describe('Todos tracker', function() {
 
   it('can mark a ToDo as complete', function(){
     browser.get('/');
-    var todo = $$('#todos p').last();
+    var todo = $$('#todos tr').last();
     todo.element(by.css('.complete')).click();
 
     expect(todo.getText()).toMatch("ToDo2: completed");
+  });
+
+  it('can search todos by name', function(){
+    browser.get('/');
+    var searchText = element(by.model('searchText'));
+    searchText.clear();
+    searchText.sendKeys('ToDo1');
+    var todos = $$('#todos tr').last().getText();
+    expect(todos).toMatch("ToDo1: completed");
   });
 
   afterEach(function(){
